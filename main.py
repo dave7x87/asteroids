818367a5a0214b201c612293861a167a582d297c
dave7x87 <dave7x05@gmail.com>
2025-11-09 21:47:13 +0000

    revised update/draw logic for groups

--- main.py
+++ main.py
@@ -7,8 +7,15 @@ from player import Player
 
 def main():
     pygame.init()
+    
     clock = pygame.time.Clock()
     dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
@@ -20,8 +27,9 @@ def main():
                 return
     
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for item_to_draw in drawable:
+            item_to_draw.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60.0) / 1000)
 
