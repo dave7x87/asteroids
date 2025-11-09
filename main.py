252764a9370c9f1f9c60459c924cfff595ca85d6
dave7x87 <dave7x05@gmail.com>
2025-11-09 20:50:28 +0000

    add FPS control

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -17,6 +19,8 @@ def main():
     
         screen.fill("black")
         pygame.display.flip()
+        dt += (clock.tick(60.0) / 1000)
+
 
 if __name__ == "__main__":
     main()
